c44c7b88125149acf6e83321a1771df9df38c7f7
lm902 <i@lm902.cn>
2018-04-27 01:39:38 +0800

    实现 Windows 启动器

---
+++ PreventMajorUpdate.Launcher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace PreventMajorUpdate.Launcher
+{
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var startInfo = new ProcessStartInfo("node.exe", "app.min.js");
+            startInfo.CreateNoWindow = true;
+            Process.Start(startInfo).WaitForExit();
+            MessageBox.Show("已阻止 Windows 大版本更新的安装。\r\nPrevented Windows from installing major release updates.", "完成", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
